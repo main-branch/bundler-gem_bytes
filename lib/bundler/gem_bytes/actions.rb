6587b7eee818444ac98ea3e0f16e29944f638fdf
James Couball <jcouball@yahoo.com>
2024-10-30 09:39:14 -0700

    feat: make #add_dependency available for gembytes scripts
    Add the #add_dependency method the actions class so it is available to gembytes scripts.

--- lib/bundler/gem_bytes/actions.rb
+++ lib/bundler/gem_bytes/actions.rb
@@ -1,9 +1,37 @@
 # frozen_string_literal: true
 
+require 'bundler/gem_bytes'
+
 module Bundler
   module GemBytes
     # The API for GemBytes templates
     # @api public
-    module Actions; end
+    module Actions
+      # Adds (or updates) a dependency in the project's gemspec file
+      #
+      # @example
+      #   add_dependency(:development, 'rspec', '~> 3.13')
+      #   add_dependency(:runtime, 'activesupport', '>= 6.0.0')
+      #
+      # @param dependency_type [Symbol] the type of dependency to add (either :development or :runtime)
+      # @param gem_name [String] the name of the gem to add
+      # @param version_constraint [String] the version constraint for the gem
+      # @param force [Boolean] whether to overwrite the existing dependency
+      # @param gemspec [String] the path to the gemspec file
+      #
+      # @return [void]
+      #
+      # @api public
+      #
+      def add_dependency(dependency_type, gem_name, version_constraint, force: false, gemspec: Dir['*.gemspec'].first)
+        puts 'Staring'
+        source = File.read(gemspec)
+        updated_source = Bundler::GemBytes::Gemspec::UpsertDependency.new(
+          dependency_type, gem_name, version_constraint, force: force
+        ).call(source)
+        File.write(gemspec, updated_source)
+        puts 'Ending'
+      end
+    end
   end
 end
